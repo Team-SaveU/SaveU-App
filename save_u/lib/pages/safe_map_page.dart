32688396493c3e0a0fa09141c28d6c2dee052d9e
jioniy <jw010208@gmail.com>
2022-12-08 21:24:15 +0900

    [ADD] #10 성북구 내 AED 및 응급실 임시 위치 정보 추가

--- save_u/lib/pages/safe_map_page.dart
+++ save_u/lib/pages/safe_map_page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-//import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:save_u/constants.dart';
 import 'package:http/http.dart' as http;
@@ -31,11 +30,86 @@ class _SafeMapPageState extends State<SafeMapPage> {
       position: LatLng(37.589667551557, 127.019849627),
       infoWindow: InfoWindow(title: 'My Position', snippet: 'where am I?'),
     ));
-    _markers_earthquake.add(Marker(
-      markerId: MarkerId('Korea Univ.'),
-      position: LatLng(37.588556165888, 127.01998189277),
-      infoWindow: InfoWindow(title: 'Korea Univ.'),
+    //성북구 지진옥외대피장소
+
+    //성북구 AED 위치
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED1'),
+      position: LatLng(37.599144919576, 127.01373838345),
+      infoWindow: InfoWindow(title: '돈암코오롱하늘채아파트(관리동 입구)'),
     ));
+
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED2'),
+      position: LatLng(37.595893281731, 127.00866801673),
+      infoWindow:
+          InfoWindow(title: '성북구민헬스센터 (지하1층 헬스장)', snippet: '성북구민회관 지하1층'),
+    ));
+
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED3'),
+      position: LatLng(37.590497458678, 127.02215700068),
+      infoWindow: InfoWindow(title: '성신여자대학교 (성신건강관리센터 입구)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED4'),
+      position: LatLng(37.591181, 127.022866),
+      infoWindow: InfoWindow(title: '성신여자대학교 (수정관 로비)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED5'),
+      position: LatLng(37.591839, 127.021731),
+      infoWindow: InfoWindow(title: '성신여자대학교 (체육관)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED6'),
+      position: LatLng(37.595151958021, 127.02316933079),
+      infoWindow: InfoWindow(title: '성신여자중학교 (4층 복도)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED7'),
+      position: LatLng(37.591091734683, 127.00704666517),
+      infoWindow: InfoWindow(title: '해오름스포츠센터 (1층 출입구 우측)', snippet: '한신한진아파트'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED8'),
+      position: LatLng(37.597487327483, 127.01245719545),
+      infoWindow: InfoWindow(title: '동소문현대아파트 (정문경비실)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED9'),
+      position: LatLng(37.594293317429, 127.02492985037),
+      infoWindow: InfoWindow(title: '개운중학교 (본관3층교무실옆)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED10'),
+      position: LatLng(37.599798492761, 127.01376011727),
+      infoWindow: InfoWindow(title: '아리랑시네센터 (1층 정문 내 2관 상영관 앞)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED11'),
+      position: LatLng(37.597275036253, 127.01237510243),
+      infoWindow: InfoWindow(title: '경찰(돈암지구대) (소내책상컴퓨터옆)'),
+    ));
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED12'),
+      position: LatLng(37.597275036253, 127.01237510243),
+      infoWindow: InfoWindow(title: '돈암치안센터 (순찰차11호 998무)'),
+    ));
+
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED13'),
+      position: LatLng(37.595151958021, 127.02316933079),
+      infoWindow: InfoWindow(title: '성신초등학교 (본관2층 통로벽)'),
+    ));
+
+    _markers_AED.add(Marker(
+      markerId: MarkerId('AED14'),
+      position: LatLng(37.594487663036, 127.02246669525),
+      infoWindow: InfoWindow(title: '성신여자고등학교 (본관 1층 교무실 앞)'),
+    ));
+
+    //성북구 응급실
     _markers_hospital.add(Marker(
       markerId: MarkerId('hospital1'),
       position: LatLng(37.596838, 127.035598),
@@ -43,20 +117,20 @@ class _SafeMapPageState extends State<SafeMapPage> {
     ));
     _markers_hospital.add(Marker(
       markerId: MarkerId('hospital2'),
-      position: LatLng(127.026508, 37.587191),
+      position: LatLng(37.586497162966, 127.02745249685),
       infoWindow: InfoWindow(title: '고려대 부속 안암 병원'),
     ));
-    /*_markers_hospital.add(Marker(
-      markerId: MarkerId('hospital2'),
-      position: LatLng(127.0073736, 37.5892371),
+    _markers_hospital.add(Marker(
+      markerId: MarkerId('hospital3'),
+      position: LatLng(37.5892371, 127.0073736),
       infoWindow: InfoWindow(title: '리누스 병원'),
     ));
-    
+
     _markers_hospital.add(Marker(
       markerId: MarkerId('hospital4'),
-      position: LatLng(127.0356232, 37.5967276),
+      position: LatLng(37.596747485193, 127.03560357939),
       infoWindow: InfoWindow(title: '성북 중앙 병원'),
-    ));*/
+    ));
   }
 
   /*_setInitialPosition() async {
